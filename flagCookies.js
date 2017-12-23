360753c582d94ad806a01e4073375002dc816a40
jan@dwrox.net <jan@dwrox.net>
2017-12-23 13:21:44 +0100

    pdate to version 1.2
    This version has been restyled and should be more pleasent to use, this was done by help from @Cybergreek at https://ngb.to !
    
    "All cookies" has been renamed to "Active cookies" as those are the cookies, which are currently active for the domain.
    "Flagged cookies" still lists "all flagged cookies for the domain", but finally has the option to remove cookies, which are not active at preset, but set to be flagged, which otherwise would be hidden.
    
    Also a search is included, which filters "Active cookies" and "Flagged cookies" easily.

--- flagCookies.js
+++ flagCookies.js
@@ -16,24 +16,33 @@ async function showCookiesForTab(tabs) {
 
     //set the header of the panel
     let activeTabUrl = document.querySelector("#header-title");
-    let text = document.createTextNode("Cookies: "+domainURL);
+    let introSpan = document.createElement('span');
+    introSpan.className = "intro";
+
+    let intro = document.createTextNode("Cookies for domain:");
+    introSpan.appendChild(intro);
+
+    let url = document.createTextNode(domainURL);
+    url.className = "domainurl";
+    activeTabUrl.appendChild(introSpan);
+    activeTabUrl.appendChild(url);
+
     let cookieList = document.getElementById('cookie-list');
     let flaggedCookieList = document.getElementById('cookie-list-flagged');
-    activeTabUrl.appendChild(text);
 
     if (cookies.length > 0) {
       for (let cookie of cookies) {
         let li = document.createElement("li");
 
-        let btn = document.createElement("button");
-        let btnContent = document.createTextNode("Flag");
-        btn.appendChild(btnContent);
-        btn.addEventListener("click", flagSwitch);
-        btn.dataset['name'] = cookie.name;
-        btn.dataset['value'] = cookie.value;
+        let checkMark = document.createElement("button");
+        checkMark.className = "checkmark";
+
+        checkMark.addEventListener("click", flagSwitch);
+        checkMark.dataset['name'] = cookie.name;
+        checkMark.dataset['value'] = cookie.value;
 
         if (data[domainURL] !== undefined && data[domainURL][cookie.name]) {
-          btn.className = "flagged";
+          checkMark.className = "checkmark flagged";
           addFlaggedCookie(cookie.name, cookie.value);
         }
 
@@ -52,7 +61,7 @@ async function showCookiesForTab(tabs) {
         p.appendChild(pCookieKeyElm);
         p.appendChild(pCookieValueElm);
 
-        li.appendChild(btn);
+        li.appendChild(checkMark);
         li.appendChild(p);
 
         cookieList.appendChild(li);
@@ -60,11 +69,11 @@ async function showCookiesForTab(tabs) {
 
     } else {
       let p = document.createElement("p");
-      let content = document.createTextNode("No cookies in this tab.");
-      let parent = cookieList.parentNode;
 
+      let content = document.createTextNode("No cookies in this tab.");
       p.appendChild(content);
-      parent.appendChild(p);
+
+      cookieList.parentNode.appendChild(p);
     }
 
     if (data[domainURL] !== undefined) {
@@ -95,6 +104,12 @@ function addFlaggedCookie(name, value) {
   let li = document.createElement("li");
   li.dataset['name'] = name;
 
+  let checkMark = document.createElement("button");
+  checkMark.className = "checkmark flagged";
+  checkMark.dataset['name'] = name;
+  checkMark.dataset['value'] = value;
+  checkMark.addEventListener("click", flagSwitch);
+
   let p = document.createElement("p");
 
   let pCookieKeyElm= document.createElement("span");
@@ -111,6 +126,7 @@ function addFlaggedCookie(name, value) {
     p.appendChild(pCookieValueElm);
   }
 
+  li.appendChild(checkMark);
   li.appendChild(p);
   flaggedCookieList.appendChild(li);
 }
@@ -118,7 +134,6 @@ function addFlaggedCookie(name, value) {
 function getActiveTab() {
   return browser.tabs.query({currentWindow: true, active: true});
 }
-
 
 //---------------------------------------------------------------------------------------------------------------------------------
 // Button switch function and store delete cookie name in browser storage
@@ -134,20 +149,28 @@ async function flagSwitch(event) {
 
   if (data[domainURL][cookieName]) {
     delete data[domainURL][cookieName];
-    event.target.className = "";
+    event.target.className = "checkmark";
 
+    // Remove from flagged list
     let flaggedCookieList = document.getElementById('cookie-list-flagged');
-    for (let index in flaggedCookieList.children) {
-      let child = flaggedCookieList.children[index];
+    for (let child of flaggedCookieList.children) {
       if (child.dataset['name'] == cookieName) {
         child.parentNode.removeChild(child);
         break;
       }
     }
 
+    // Uncheck from flagged in active cookies, if present
+    let domainCookieList = document.getElementById('cookie-list');
+    for (let child of domainCookieList.children) {
+      if (child.children[0].dataset['name'] == cookieName) {
+        child.children[0].className = "checkmark";
+        break;
+      }
+    }
   } else {
     data[domainURL][cookieName] = true;
-    event.target.className = "flagged";
+    event.target.className = "checkmark flagged";
     addFlaggedCookie(cookieName, cookieValue);
   }
 
@@ -155,18 +178,60 @@ async function flagSwitch(event) {
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------
+// Switches for main buttons
+function unhide(targetList) {
+  let searchVal = document.getElementById("searchBar").value.trim().toLowerCase();
+  if (searchVal.length != 0) {
+    doSearch(searchVal);
+  } else {
+    for (let child of targetList.children) {
+      if (child.className == "hidden") {
+        child.className = "";
+      }
+    }
+  }
+}
+
 function switchFlagged() {
+  let list = document.getElementById("cookie-list-flagged");
+  unhide(list);
+
   document.getElementById("cookie-list").className = "hidden";
-  document.getElementById("cookie-list-flagged").className = "";
+  list.className = "";
 }
 
 function switchAll() {
-  document.getElementById("cookie-list").className = "";
+  let list = document.getElementById("cookie-list");
+  unhide(list);
+
   document.getElementById("cookie-list-flagged").className = "hidden";
+  list.className = "";
+}
+
+function searchContent(event) {
+  let searchVal = event.target.value.trim().toLowerCase();
+  doSearch(searchVal, "cookie-list");
+  doSearch(searchVal, "cookie-list-flagged");
+}
+
+function doSearch(searchVal, targetList) {
+  let searchTarget = document.getElementById(targetList);
+  for (let child of searchTarget.children) {
+    let contentChild = child.children[0];
+    let cookieKey = contentChild.dataset["name"].toLowerCase();
+    let cookieValue = contentChild.dataset["value"].toLowerCase();
+    if (cookieKey.indexOf(searchVal) == -1 && cookieValue.indexOf(searchVal) == -1) {
+        child.className = "hidden";
+    } else {
+      child.className = "";
+    }
+  }
 }
 
 // Startup code
 
 document.getElementById("flaggedCookies").addEventListener("click", switchFlagged);
 document.getElementById("allCookies").addEventListener("click", switchAll);
+document.getElementById("searchBar").addEventListener("keyup", searchContent);
+
 getActiveTab().then(showCookiesForTab);
